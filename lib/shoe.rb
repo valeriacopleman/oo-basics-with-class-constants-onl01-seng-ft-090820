ff502960fc351cd6e792161e7cb5c215db2f9a02
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-21 16:05:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,10 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+<<<<<<< HEAD
   BRANDS = []
+=======
+>>>>>>> 77677ab2a931d15c2e81cbc8a846e0086d33f3d1
 
   def initialize(brand)
     @brand = brand
@@ -18,10 +21,19 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
+<<<<<<< HEAD
   
   def brand=(brand)
     @brand = brand
   end
     
+=======
+  BRANDS = []
+  
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand 
+  end
+>>>>>>> 77677ab2a931d15c2e81cbc8a846e0086d33f3d1
 
 end
